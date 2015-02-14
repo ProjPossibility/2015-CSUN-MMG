737b7fe8f9153674f450b6da6ada7447edc756fe
Cameron McAteer <cmcateer@vrinc.com>
2015-02-14 13:26:22 -0800

    Created basic main menu for game. Added exit button to close app.

--- app/src/main/java/com/ss12/csun_mmg/peripheralmaze/MainGame.java
+++ app/src/main/java/com/ss12/csun_mmg/peripheralmaze/MainGame.java
@@ -112,7 +112,13 @@ public class MainGame extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.btn_exit).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                finish();
+                return true;
+            }
+        });
     }
 
     @Override
