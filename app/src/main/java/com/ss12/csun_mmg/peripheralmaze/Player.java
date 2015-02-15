c622e180e0ff239b70224245ff17d5ae115c3ad2
Gustavo Arce <gustavo.arce.719@my.csun.edu>
2015-02-15 13:58:39 -0800

    Revert "- facing variable is updated on movement."
    This reverts commit 461737edb0662bdfa0163993c9d47b8da6d28647.

--- app/src/main/java/com/ss12/csun_mmg/peripheralmaze/Player.java
+++ app/src/main/java/com/ss12/csun_mmg/peripheralmaze/Player.java
@@ -7,57 +7,50 @@ import android.os.Vibrator;
  * Created by Guss on 2/15/2015.
  */
 public class Player {
-    public String location;
-    public int facing;
-    public Tile currentTile;
-    public int stepCounter;
+    private String location;
+    private int facing;
+    private Tile currentTile;
 
     public Player (String l, int f) {
         location = l;
         facing = f;
         currentTile = getTileFromLocation(l);
-        stepCounter = 0;
     }
 
-    public boolean look(int direction) {
-        stepCounter += 1;
-        return checkIfWall(direction);
-    }
-
-    public boolean move(int direction) {
-        facing = direction;
-
-        if (checkIfWall(direction))
-            return false;
-
-        int x = Integer.parseInt(""+location.charAt(0));
-        int y = Integer.parseInt(""+location.charAt(1));
-
-        switch (direction) {
-            case 0:
-                this.updateLocation((x-1) + "" + y);
-                break;
-            case 1:
-                this.updateLocation(x + "" + (y+1));
-                break;
-            case 2:
-                this.updateLocation((x+1) + "" + y);
-                break;
-            case 3:
-                this.updateLocation(x + "" + (y-1));
-                break;
-        }
-        stepCounter += 2;
-        return true;
-    }
-
-    private void updateLocation(String newLocation) {
+    public void updateLocation(String newLocation) {
         location = newLocation;
         currentTile = getTileFromLocation(newLocation);
     }
 
-    private boolean checkIfWall(int direction) {
+    public boolean checkIfWall(int direction) {
         return (currentTile.walls[direction] > 0);
+    }
+
+    public void look(Context context, int direction) {
+        vibrate(context, checkIfWall(direction));
+    }
+
+    public void move(int direction) {
+        if (!checkIfWall(direction))
+        {
+            int x = Integer.parseInt(""+location.charAt(0));
+            int y = Integer.parseInt(""+location.charAt(1));
+
+            switch (direction) {
+                case 0:
+                    this.updateLocation((x-1) + "" + y);
+                    break;
+                case 1:
+                    this.updateLocation(x + "" + (y+1));
+                    break;
+                case 2:
+                    this.updateLocation((x+1) + "" + y);
+                    break;
+                case 3:
+                    this.updateLocation(x + "" + (y-1));
+                    break;
+            }
+        }
     }
 
     private Tile getTileFromLocation(String location) {
@@ -66,8 +59,7 @@ public class Player {
 
     private void vibrate(Context c, boolean b) {
         long[] pattern;
-        pattern = (b) ? new long[]{0,200,100,500} : new long[]{0,50};
+        pattern = (b) ? new long[]{0,200,100,500} : new long[]{0,1};
         Vibrator v = (Vibrator) c.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(pattern,1);
     }
 }
